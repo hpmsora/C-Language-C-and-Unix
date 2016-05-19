a869aea99454040e6ca82ef3b416479750ec6443
Wonyong Ha <woha@indiana.edu>
2016-05-19 14:00:06 -0400

    Fixed

--- Assignment_01/A1deprec.c
+++ Assignment_01/A1deprec.c
@@ -11,15 +11,15 @@
 int main() {
 
   //declare variable for storing value
-  int p_price; //price value
-  float years, depre; //years and depre value
+  int years; //price value
+  float p_price, depre; //years and depre value
 
   //asking value and receiving
   printf("Enter the purchase price, years of service, annual depreciation:\n");
-  scanf("%d %f %f", &p_price, &years, &depre); //getting and storing value
+  scanf("%f %d %f", &p_price, &years, &depre); //getting and storing value
 
   //calculating and print the value
-  printf("The salvage value of the item is %f\n", p_price -  (depre * years));
+  printf("The salvage value of the item is %f\n", p_price - (depre * years));
 
   //finishing program
   return 0;
