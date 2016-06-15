9c13576bec7f8fa0a9630cb0edc697279104fcd8
Wonyong Ha <woha@indiana.edu>
2016-06-15 13:29:22 -0400

    Assignment 4 Back up June 16 2016

---
+++ Assignment_04/A4.c
@@ -0,0 +1,37 @@
+/*Filename: A4.c *
+*Part of: Fourth A290 Assignment *
+*Created by: Won Yong Ha *
+*Created on: 6/13/2016 *
+*Last Modified by: Won Yong Ha *
+*Last Modified on: 6/16/2016 *
+*/ 
+
+/*Importing Libraries*/
+#include <stdio.h>
+
+/*printOption Function*/
+void printOption() {
+
+  /*Print the options*/
+  printf("1.  Insert a Node at the end,\n");
+  printf("2.  Insert a Node at the beginning,\n");
+  printf("3.  Delete a Node from the end,\n");
+  printf("4.  Delete a Node from the beginning,\n"); 
+  printf("5.  Insert a Node in the middle (nth position),\n");
+  printf("6.  Delete a Node from the middle (nth position), and\n");
+  printf("7.  Modify the int value of any Node currently in the list.\n");
+  printf("8.  Exit gracefully after as many operations as the user chooses.\n");
+
+}
+
+/*main Function*/
+int main() {
+
+  /*Declare variables*/
+  int option = 0;
+
+  /**/
+
+  /*Finishing the function*/
+  return 0;
+}
